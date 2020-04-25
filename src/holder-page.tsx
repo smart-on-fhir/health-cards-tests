dbf8448847a6460ff9d3325edc487a39a7c3f164
Josh Mandel <jmandel@med.harvard.edu>
2020-04-25 11:56:27 -0500

    Add UI

--- src/holder-page.tsx
+++ src/holder-page.tsx
@@ -3,6 +3,10 @@ import { simulatedOccurrence, verifierWorld } from './verifier'
 import { issuerWorld } from './issuer'
 import React, { useState, useEffect, useReducer, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import * as RS from 'reactstrap';
+import { Navbar, NavbarBrand, NavItem, DropdownItem, DropdownMenu, UncontrolledDropdown, NavbarToggler, Nav, NavbarText, Collapse, NavLink, DropdownToggle, Card, Button, CardSubtitle, CardTitle, CardText } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import QrScanner from 'qr-scanner';
 QrScanner.WORKER_PATH = 'qr-scanner-worker.min.js';
@@ -38,7 +42,7 @@ const QRScanner: React.FC<{ label: string, onScanned: (s: string) => void, simul
     return <>
         <span>Scan barcode for {props.label}</span><br />
         <video ref={videoRef} style={{ width: "25vmin", height: "25vmin" }}></video>
-        <br/>
+        <br />
     </>
 }
 
@@ -55,7 +59,7 @@ const App: React.FC<{ initialState: HolderState, simulatedBarcodeScan: boolean }
     }
 
     const connectTo = who => async () => {
-        dispatchToHolder({ 'type': 'begin-interaction', who})
+        dispatchToHolder({ 'type': 'begin-interaction', who })
     }
 
     const retrieveVcClick = async () => {
@@ -73,23 +77,73 @@ const App: React.FC<{ initialState: HolderState, simulatedBarcodeScan: boolean }
     const showIssuerApproval = (interaction && holderState.interactions.length == 1 && interaction.status === "need-approval")
     const showVerifierApproval = (interaction && holderState.interactions.length == 2 && interaction.status === "need-approval")
 
-    return <>
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    return <div style={{paddingTop: "5em"}}>
+        
+        <RS.Container>
+            <RS.Row>
+                
+                <RS.Col xs="12">
+                    <RS.Navbar expand="" className="navbar-dark bg-info fixed-top">
+                        <NavbarBrand style={{ marginRight: "2em" }} href="/">
+                            <img className="d-inline-block" style={{ maxHeight: "1em", maxWidth: "1em", marginRight: "10px" }} src="img/wallet.svg" />
+                            Health Wallet Demo
+                    </NavbarBrand>
+                        <NavbarToggler onClick={toggle}></NavbarToggler>
+                        <Collapse navbar>
+                            <Nav navbar>
+                                <NavbarText>Help</NavbarText>
+                                <NavbarText>About</NavbarText>
+                            </Nav>
+                        </Collapse>
+                    </RS.Navbar>
+                </RS.Col>
+            </RS.Row>
+            <RS.Row>
+                <RS.Col xs="12">
+                <Card style={{border: "1px solid grey", padding: ".5em", marginBottom: "1em"}}>
+                    <CardTitle>
+                        Your Decentralized Identifier (DID)
+                    </CardTitle>
+                        <CardSubtitle className="text-muted">This identifier is all yours, with keys stored safely on your device.</CardSubtitle>
+<CardText style={{fontFamily: "monospace"}}> {holderState.did.split('?')[0]}</CardText>
+                </Card>
+ 
+                <Card style={{border: "1px dashed grey", padding: ".5em"}}>
+                    <CardTitle>
+                        COVID Card
+                    </CardTitle>
+                        <CardSubtitle className="text-muted">You don't have a COVID card in your wallet yet.</CardSubtitle>
+                        <CardText></CardText>
+                        <Button disabled className="mb-1" color="info">✓ 1. Set up your Health Wallet</Button>
+                        <Button className="mb-1" color="success"> 2. Find a lab</Button>
+                        <Button disabled className="mb-1" color="info">3. Get tested</Button>
+                    <Button disabled className="mb-1" color="info">4. Download your results</Button>
+                </Card>
+                </RS.Col>
+            </RS.Row>
+        </RS.Container>
+    </div>
+}
+/*
         {!props.simulatedBarcodeScan ? <><a href=".?simulate-barcode">Simulate barcode scans</a> {" | "}</> : ""}
         <a target="_blank" href="./issuer">Open Lab Demo</a> {" | "}
-        <a target="_blank" href="./verifier">Open Verifier Demo</a> <br/>
-        {interaction && interaction.status === "need-qrcode" && <QRScanner 
+        <a target="_blank" href="./verifier">Open Verifier Demo</a> <br />
+        {interaction && interaction.status === "need-qrcode" && <QRScanner
             onScanned={onScanned}
             label="Lab"
-            simulatedScan={props.simulatedBarcodeScan ? interaction : null}/>}
+            simulatedScan={props.simulatedBarcodeScan ? interaction : null} />}
         <button onClick={connectTo('issuer')}>Connect to Lab</button> <br />
         <button onClick={onApproval('issuer')} disabled={!showIssuerApproval}>Approve sharing my identity</button> <br />
-        <button onClick={retrieveVcClick}>Get Health Card from Lab</button> 
+        <button onClick={retrieveVcClick}>Get Health Card from Lab</button>
         {" "} Currently in VC Store: {holderState.vcStore.length}<br />
         <button onClick={connectTo('verifier')}>Present Health Card</button> <br />
         <button onClick={onApproval('verifier')} disabled={!showVerifierApproval}>Approving sharing my health card</button> <br />
         <pre> {JSON.stringify(holderState, null, 2)} </pre>
-    </>
-}
+
+*/
 
 export default async function main() {
     const simulatedBarcodeScan = !!window.location.search.match(/simulate-barcode/)
