4c2317db1e455befd41510a10dafe9cb207dfd1a
ssffnet <larryjoyjr@outlook.com>
2021-12-01 15:39:55 -0800

    Fix issue where blocked camera causes error

--- demo-portals/public/qrscanner.js
+++ demo-portals/public/qrscanner.js
@@ -33,13 +33,17 @@ const QrScanner = function (vidId) {
             promise.resolve = resolve;
             promise.reject = reject;
             // Use facingMode: environment to attempt to get the front camera on phones
-            navigator.mediaDevices.getUserMedia({ video: { width: 600, facingMode: "environment" } }).then(function (stream) {
+            navigator.mediaDevices.getUserMedia({ video: { width: 600, facingMode: "environment" } })
+                .then(function (stream) {
                 video.srcObject = stream;
                 video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
                 video.play();
                 vidStream = stream;
 
                 requestAnimationFrame(tick);
+                })
+                .catch(function (error) {
+                    resolve({ error: error.message });
             });
         });
     }
